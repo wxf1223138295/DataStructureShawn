0aa5f8f6a4ae5b1b5199ef63a46d7f6963392c9c
XiaofengWang <1223138295@qq.com>
2020-12-15 21:21:55 +0800

    堆排序

---
+++ SortSummary/堆排序/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace 堆排序
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //声明数据进行相应的测试
+            int[] myArray = new int[] { 45, 36, 18, 53, 72, 30, 48, 93, 15, 36 };
+            
+            //创建树
+            CreateTree(myArray);
+            
+        }
+
+        public static void CreateTree(int[] arry)
+        {
+            //完全二叉树 最下面一排  个数等于上面之和
+            //所以中间的那个点是完全二叉树的最后一个根节点
+
+            for (int i = arry.Length / 2 - 1; i > 0; i--)
+            {
+                SelectMax(arry,i,arry.Length);
+            }
+        }
+
+        public static void SelectMax(int[] array,int rootindex,int heapSize)
+        {
+            int left = 2 * rootindex + 1;
+            int right = 2 * rootindex + 2;
+            int large = rootindex;
+            if (left < heapSize && array[left] > array[large])  //与左子节点进行比较
+            {
+                large = left;
+            }
+            if (right < heapSize && array[right] > array[large])    //与右子节点进行比较
+            {
+                large = right;
+            }
+
+            if (rootindex != large)//有变化
+            {
+                //当前根节点和large这个节点换位置
+                //交换数值
+                
+            }
+        }
+    }
+}
