70f2abd1546700ec27a72a19047c0201c32d20cd
wangxiaofeng <wang.xiaofeng@synyi.com>
2020-12-21 18:11:47 +0800

    回溯算法

---
+++ SortSummary/46. 全排列/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _46._全排列
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arry = new int[] {1, 2, 3};
+            Solution s=new Solution();
+            s.Permute(arry);
+        }
+    }
+
+    public class Solution
+    {
+        public IList<IList<int>> results=new List<IList<int>>();
+        public IList<IList<int>> Permute(int[] nums)
+        { 
+            // 记录「路径」
+            List<int> track=new List<int>();
+            TrackBack(nums, track);
+            return results;
+        }
+        // 路径：记录在 track 中
+        // 选择列表：nums 中不存在于 track 的那些元素
+        // 结束条件：nums 中的元素全都在 track 中出现
+        private void TrackBack(int[] nums,List<int> list)
+        {
+            if (list.Count == nums.Length)
+            {
+                results.Add(new List<int>(list));
+                return;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                // 排除不合法的选择
+                if (list.Contains(nums[i]))
+                    continue;
+                list.Add(nums[i]);
+
+                TrackBack(nums, list);
+
+                list.Remove(list.Last());
+            }
+
+        }
+    }
+}
---
+++ SortSummary/N皇后/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace N皇后
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s=new Solution();
+            s.SolveNQueens(8);
+        }
+    }
+    public class Solution
+    {
+
+        public IList<IList<string>> results = new List<IList<string>>();
+
+        //列
+        public int MaxWith = 0;
+        //行
+        public int Maxheight = 0;
+        public IList<IList<string>> SolveNQueens(int n)
+        {
+            //创建N*N的棋盘
+            string[,] sd=new string[n,n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    sd[i,j] = ".";
+                }
+            }
+
+            MaxWith = n;
+            Maxheight = n;
+            
+
+
+
+
+            BackTrack(sd, 0);
+
+            return results;
+        }
+
+        private void BackTrack(string[,] s, int row)
+        {
+            if (row == Maxheight)
+            {
+                List<string> temp = new List<string>();
+                for (var i = 0; i < MaxWith; i++)
+                {
+                    var t = string.Empty;
+                   for (int j = 0; j < row; j++)
+                   {
+                       t = t + s[i,j];
+                   }
+                   temp.Add(t);
+                }
+                results.Add(temp);
+                return;
+            }
+
+            //第row行的个数 即列数
+
+            for (int col = 0; col < MaxWith; col++)
+            {
+                //排除不合法的
+               if(!isValid(s, row, col)) continue ;
+
+               s[row,col] = "Q";
+               BackTrack(s, row + 1);
+               s[row,col] = ".";
+            }
+        }
+
+        /// <summary>
+        /// 是否可以在 board[row][col] 放置皇后
+        /// </summary>
+        /// <param name="board"></param>
+        /// <param name="row"></param>
+        /// <param name="col"></param>
+        /// <returns></returns>
+        bool isValid(string[,] board, int row, int col)
+        {
+         
+            for (int i = 0; i < Maxheight; i++)
+            {
+                if (board[i,col]=="Q")
+                {
+                    return false;
+                }
+            }
+
+            //左上
+            for (int i = row-1,j=col-1; i>=0&&j>=0; i--,j--)
+            {
+                if (board[i,j] == "Q")
+                    return false;
+            }
+
+
+            //右上
+            for (int i = row - 1, j = col + 1; i >= 0 && j < Maxheight; i--, j++)
+            {
+                if (board[i,j] == "Q")
+                    return false;
+
+            }
+
+            return true;
+        }
+    }
+}
