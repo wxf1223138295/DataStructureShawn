de75e0d1103ee7fefe87d6b5194b48b0529a4bd2
wangxiaofeng <wang.xiaofeng@synyi.com>
2021-03-19 17:41:14 +0800

    盛最多水的容器

---
+++ SortSummary/_11.盛最多水的容器/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _11.盛最多水的容器
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s=new Solution();
+            s.MaxArea(new[] {1, 1});
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class Solution {
+        
+        public int MaxArea(int[] height)
+        {
+            int l = 0;
+            int r = height.Length - 1;
+            var maxplace = 0;
+            while (l<r)
+            {
+                var heightvalue = Math.Min(height[l],height[r]);
+                var width = r-l;
+                maxplace = Math.Max(maxplace,heightvalue*width);
+
+                if (height[l] < height[r])
+                {
+                    l++;
+                }
+                else
+                {
+                    r--;
+                }
+                
+            }
+
+            return maxplace;
+        }
+    }
+}
